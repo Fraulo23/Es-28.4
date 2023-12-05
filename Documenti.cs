b0314541b1657d2e70dac238a3b9439fd3b396ae
Fraulo23 <fraulinifederico@gmail.com>
2023-12-05 18:06:42 +0100

    Aggiungere i file di progetto.

---
+++ Documenti.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_28._4
+{
+    enum Stato
+    {
+        InPrestito,
+        Disponibile
+    }
+    internal class Documenti
+    {
+        public string Codice { get; set; }
+        public string Titolo { get; set; }
+        public DateTime Anno { get; set; }
+        public string Settore { get; set; }
+        public Stato stato { get; set; }
+        public string PosizioneScaffale { get; set; }
+        public string Autore { get; set; }
+
+    }
+}
---
+++ Prestito.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_28._4
+{
+    internal class Prestito
+    {
+        public DateTime DA { get; set; }
+        public DateTime A { get; set; }
+        public string Codice { get; set; }
+    }
+}
